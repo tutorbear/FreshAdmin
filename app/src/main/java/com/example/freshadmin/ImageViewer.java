42eea7eb771d1bd68c399e097ed03e120d993e43
Lassassin <sanjid8426@gmail.com>
2021-04-22 21:45:34 +0600

    joom

---
+++ app/src/main/java/com/example/freshadmin/ImageViewer.java
@@ -0,0 +1,13 @@
+package com.example.freshadmin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ImageViewer extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_viewer);
+    }
+}
